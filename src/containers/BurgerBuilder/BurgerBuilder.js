2bc326f31ffc4d270c4e05b8711ace96cf50e534
Prokopenko <prokopenko.cnw@gmail.com>
2018-04-16 14:35:52 +0300

    Burger reduce

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component{
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 3,
+            bacon: 2,
+            cheese: 1,
+            meat: 1
         }
     }
     render(){
